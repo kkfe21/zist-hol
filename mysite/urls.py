aec060335260ab361dae49513eceedd287b0aae7
hyewonkong <hyewonkong@microsoft.com>
2023-09-05 08:33:11 +0900

    add polls app

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,4 +21,5 @@ from main import views as main_views
 urlpatterns = [
     path('',main_views.index, name="index"),
     path('admin/', admin.site.urls),
+    path('polls,', include("polls.urls"))
 ]
